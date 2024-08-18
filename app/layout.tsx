587dcd313ee9a091859203620077fa056a3d51d0
Raphael Didjurgeit <raphael.didjurgeit@joblocal.de>
2024-08-18 11:09:10 +0200

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Bills',
+    default: 'Bills',
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
