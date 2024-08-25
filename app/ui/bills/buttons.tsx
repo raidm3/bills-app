9b3390a951ee4bf5b89930e0e7f5565a03078497
Raphael Didjurgeit <raphael.didjurgeit@joblocal.de>
2024-08-25 16:24:01 +0200

    improve button click feedback

--- app/ui/bills/buttons.tsx
+++ app/ui/bills/buttons.tsx
@@ -1,11 +1,21 @@
+'use client';
+
 import { PencilIcon, PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export function CreateBill() {
+  let pending = false;
   return (
     <Link
       href="/dashboard/bills/create"
-      className="flex h-10 items-center rounded-lg bg-primary px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      onClick={() => { pending = true; }}
+      className={clsx(
+        "flex h-10 items-center rounded-lg bg-primary px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600",
+        {
+          'bg-blue-200': pending,
+        },
+      )}
     >
       <span className="hidden md:block">Create Bill</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
